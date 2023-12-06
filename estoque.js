94aaf96d02ffdff3a027762191f73160f0e39278
Sarah Cristina <sarah.cristina@estudante.ufjf.br>
2023-12-06 20:59:24 -0300

    Cria estoque.js

---
+++ estoque.js
@@ -0,0 +1,14 @@
+
+let estoque = {
+
+joao: [
+        {tipo: 'maca', qtd: 1},
+        {tipo:'pera', qtd: 1},
+    ],
+maria: [
+        {tipo:'maca', qtd: 2},
+        {tipo:'banana', qtd: 4},
+    ],
+};export function getEstoque(){
+    struct = cloneEstoque;
+}

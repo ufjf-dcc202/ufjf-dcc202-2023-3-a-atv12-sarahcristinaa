88e4ab4f5601353ad26d7e93d604fd21bb72ba2b
Sarah Cristina <sarah.cristina@estudante.ufjf.br>
2023-12-06 20:59:45 -0300

    Cria main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+
+document.entrada.addEventListener('submit', leFormulario);
+const olJoao = document.querySelector("#olJoao");
+const olMaria = document.querySelector("#olMaria");
+function leFormulario(event){
+    event.preventDefault();
+    const fruta = document.entrada.fruta.value;
+    const quantidade = document.entrada.quantidade.value;
+    console.log( 'fruta: ${fruta} qtd: ${quantidade}');
+    //document.entrada.submit();
+    //event.submit();
+}

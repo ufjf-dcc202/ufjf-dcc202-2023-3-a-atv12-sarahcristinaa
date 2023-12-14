40b8ddf5cb6de2ac2e1ff3fc04de188322a2926d
Sarah Cristina <sara.cristina@estudante.ufjf.br>
2023-12-13 21:08:06 -0300

    Cria as funções  de transação

--- estoque.js
+++ estoque.js
@@ -1,14 +1,64 @@
+//estoque.js
 
 let estoque = {
 
-joao: [
-        {tipo: 'maca', qtd: 1},
-        {tipo:'pera', qtd: 1},
+    'joao': [
+        { 'tipo': 'maca', qtd: 1 },
+        { 'tipo': 'pera', qtd: 1 },
     ],
-maria: [
-        {tipo:'maca', qtd: 2},
-        {tipo:'banana', qtd: 4},
+    'maria': [
+        { 'tipo': 'maca', qtd: 2 },
+        { 'tipo': 'banana', qtd: 4 },
     ],
-};export function getEstoque(){
-    struct = cloneEstoque;
+};
+export function getEstoque() {
+    return structuredClone(estoque);
 }
+
+export function transacao(origem, destino, quantidade, fruta) {
+    if (origem === destino) {
+        return;
+    }
+    if (origem === 'pomar') {
+        dePomarParaPessoa(destino, quantidade, fruta);
+    }
+
+    if (destino === "pomar") {
+        dePessoaParaPomar(origem, quantidade, fruta);
+
+    }
+}
+
+function dePomarParaPessoa(destino, quantidade, fruta) {
+    const pessoa = estoque[destino];
+    let monte;
+    for (let i = 0; i < pessoa.length; i++) {
+        if (pessoa[i].tipo === fruta) {
+            monte = pessoa[i];
+            break;
+        }
+    }
+    if (!monte) {
+        monte = { 'tipo': fruta, 'qtd': 0 }
+        pessoa.push(monte);
+    }
+    monte.qtd += quantidade;
+    return;
+
+}
+function dePessoaParaPomar(origem, quantidade, fruta) {
+    const pessoa = estoque[origem];
+    let monte;
+    for (let i = 0; i < pessoa.length; i++) {
+        if (pessoa[i].tipo === fruta) {
+            monte = pessoa[i];
+            break;
+        }
+    }
+    if (!monte) {
+        return;
+    }
+    monte.qtd -= Math.min(quantidade, monte.qtd);
+}
+
+//export{getEstoque}

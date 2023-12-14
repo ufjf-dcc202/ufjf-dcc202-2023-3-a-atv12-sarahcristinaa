4bab2fe57b1cb4c5345354b4f5dc2eac53377d46
Sarah Cristina <sara.cristina@estudante.ufjf.br>
2023-12-13 21:09:01 -0300

    Cria atualiza tela e chama transação

--- main.js
+++ main.js
@@ -1,12 +1,36 @@
+//main.js
+import {getEstoque, transacao} from './estoque.js';
+
 
 document.entrada.addEventListener('submit', leFormulario);
 const olJoao = document.querySelector("#olJoao");
 const olMaria = document.querySelector("#olMaria");
+atualizaTela();
+
 function leFormulario(event){
     event.preventDefault();
     const fruta = document.entrada.fruta.value;
-    const quantidade = document.entrada.quantidade.value;
-    console.log( 'fruta: ${fruta} qtd: ${quantidade}');
+    const quantidade = document.entrada.quantidade.valueAsNumber;
+    const origem = document.entrada.origem.value;
+    const destino = document.entrada.destino.value;
+    console.log(`Solicitado: ${origem} doa ${quantidade}${fruta} para ${destino}`);
     //document.entrada.submit();
-    //event.submit();
+    
+    transacao(origem,destino,quantidade,fruta);
+    atualizaTela();
+}
+
+function atualizaTela(){
+    const estoque = getEstoque();
+    preencheListaPessoa(estoque['joao'], olJoao);
+    preencheListaPessoa(estoque['maria'], olMaria);
+}
+function preencheListaPessoa(pessoa, lista){
+    olMaria.innerHTML = " ";
+    for(let i = 0; i<pessoa.length; i++){
+        const monte = pessoa[i];
+        const eLi = document.createElement('li');
+        eLi.innerText = `${monte.tipo}: ${monte.qtd}`;
+        lista.append(eLi);
+    }
 }
